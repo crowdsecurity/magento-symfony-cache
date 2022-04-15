13661ab2d4928394623457eae12878457d6bffcd
Julien Loizelet <julienloizelet@okaeli.com>
2022-04-15 14:27:01 +0900

    feat(*): Add compatibility with psr cache 3 when running PHP 8 (used for Magento 2.4.4)

--- Adapter/AdapterInterface.php
+++ Adapter/AdapterInterface.php
@@ -17,31 +17,63 @@ use Symfony\Component\Cache\CacheItem;
 // Help opcache.preload discover always-needed symbols
 class_exists(CacheItem::class);
 
-/**
- * Interface for adapters managing instances of Symfony's CacheItem.
- *
- * @author Kévin Dunglas <dunglas@gmail.com>
- */
-interface AdapterInterface extends CacheItemPoolInterface
-{
+if (\PHP_VERSION_ID >= 80000) {
+    // We copy the 6.0.6 version on symfony/cache package
     /**
-     * {@inheritdoc}
+     * Interface for adapters managing instances of Symfony's CacheItem.
      *
-     * @return CacheItem
+     * @author Kévin Dunglas <dunglas@gmail.com>
      */
-    public function getItem($key);
+    interface AdapterInterface extends CacheItemPoolInterface
+    {
+        /**
+         * {@inheritdoc}
+         */
+        public function getItem(mixed $key): CacheItem;
 
-    /**
-     * {@inheritdoc}
-     *
-     * @return \Traversable<string, CacheItem>
-     */
-    public function getItems(array $keys = []);
+        /**
+         * {@inheritdoc}
+         *
+         * @return iterable<string, CacheItem>
+         */
+        public function getItems(array $keys = []): iterable;
 
-    /**
-     * {@inheritdoc}
-     *
-     * @return bool
-     */
-    public function clear(string $prefix = '');
+        /**
+         * {@inheritdoc}
+         */
+        public function clear(string $prefix = ''): bool;
+    }
 }
+else{
+    // We keep the 5.4.7 version on symfony/cache package
+    /**
+     * Interface for adapters managing instances of Symfony's CacheItem.
+     *
+     * @author Kévin Dunglas <dunglas@gmail.com>
+     */
+    interface AdapterInterface extends CacheItemPoolInterface
+    {
+        /**
+         * {@inheritdoc}
+         *
+         * @return CacheItem
+         */
+        public function getItem($key);
+
+        /**
+         * {@inheritdoc}
+         *
+         * @return \Traversable<string, CacheItem>
+         */
+        public function getItems(array $keys = []);
+
+        /**
+         * {@inheritdoc}
+         *
+         * @return bool
+         */
+        public function clear(string $prefix = '');
+    }
+}
+
+
