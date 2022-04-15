527ddba1b259103b88e3194bf331ab7da364af98
Julien Loizelet <julienloizelet@okaeli.com>
2022-04-15 16:50:37 +0900

    fix(*): Fix bad class name and add replace part in composer.json

--- CacheItemPhp8.php
+++ CacheItemPhp8.php
@@ -15,7 +15,7 @@ use Symfony\Component\Cache\Exception\InvalidArgumentException;
 use Symfony\Component\Cache\Exception\LogicException;
 use Symfony\Contracts\Cache\ItemInterface;
 
-final class CacheItemPhp8 implements ItemInterface
+final class CacheItem implements ItemInterface
 {
     private const METADATA_EXPIRY_OFFSET = 1527506807;
 
