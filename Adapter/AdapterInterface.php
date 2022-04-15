f605808d58c43ff7d05590710a0bb9aa25ba21ee
Julien Loizelet <julienloizelet@okaeli.com>
2022-04-15 17:55:05 +0900

    feat(php8): Prepare specific version for PHP 8

--- Adapter/AdapterInterface.php
+++ Adapter/AdapterInterface.php
@@ -1,7 +1,45 @@
 <?php
-if (\PHP_VERSION_ID >= 80000) {
-    require_once("AdapterInterfacePhp8.php");
-}
-else{
-    require_once("AdapterInterfacePhp7.php");
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Cache\Adapter;
+
+use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Component\Cache\CacheItem;
+
+// Help opcache.preload discover always-needed symbols
+class_exists(CacheItem::class);
+
+// We copy the 6.0.6 version on symfony/cache package
+
+/**
+ * Interface for adapters managing instances of Symfony's CacheItem.
+ *
+ * @author Kévin Dunglas <dunglas@gmail.com>
+ */
+interface AdapterInterface extends CacheItemPoolInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getItem(mixed $key): CacheItem;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return iterable<string, CacheItem>
+     */
+    public function getItems(array $keys = []): iterable;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function clear(string $prefix = ''): bool;
 }
